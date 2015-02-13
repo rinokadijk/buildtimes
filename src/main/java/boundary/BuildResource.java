cb74049455392b820d799c79d0f69ea91094937d
Rino <rino.kadijk@salvesdevelopment.nl>
2015-02-13 17:27:00 +0100

    safety checks

--- src/main/java/boundary/BuildResource.java
+++ src/main/java/boundary/BuildResource.java
@@ -20,6 +20,9 @@ public class BuildResource {
     @GET
     @Path("build")
     public Response saveBuild(@QueryParam(value = "script") String scriptName, @QueryParam(value = "duration") int duration, @QueryParam(value = "user") String userName, @QueryParam(value = "failure") boolean isFailure) {
+        if (scriptName == null || duration < 1 || userName == null) {
+            return Response.accepted().build();
+        }
         BuildResult build = new BuildResult(scriptName, duration, new Date(), userName, !isFailure);
         buildService.save(build);
         return Response.ok().build();
